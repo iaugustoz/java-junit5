9867180d2aaf15aa2e294e15f46229d831bbe65e
Igor Augusto <igoraugustobz@gmail.com>
2024-01-10 04:47:46 -0300

    test: deve rejeitar um usuário sem nome, e-mail e senha

--- unit-test-api/src/test/java/br/com/iaugusto/domain/UserTest.java
+++ unit-test-api/src/test/java/br/com/iaugusto/domain/UserTest.java
@@ -1,10 +1,10 @@
 package br.com.iaugusto.domain;
 
+import br.com.iaugusto.domain.exceptions.ValidationException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Domínio: Usuário")
 class UserTest {
@@ -24,4 +24,33 @@ class UserTest {
         );
     }
 
+    @Test
+    @DisplayName("Deve rejeitar um usuário sem nome")
+    void mustRejectUnnamedUser() {
+        ValidationException exception = assertThrows(ValidationException.class, () ->
+                new User(2L, null, "user2@gmail.com", "7890")
+        );
+
+        assertEquals("O campo name é obrigatório!", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Deve rejeitar um usuário sem e-mail")
+    void mustRejectUserWithoutEmail() {
+        ValidationException exception = assertThrows(ValidationException.class,
+                () -> new User(3L, "Test User", null, "123")
+        );
+
+        assertEquals("O campo e-mail é obrigatório!", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Deve rejeitar um usuário sem senha")
+    void mustRejectUserWithoutPassword() {
+        ValidationException exception = assertThrows(ValidationException.class,
+                () -> new User(4L, "User Usuário", "user3@gmail.com", null)
+        );
+
+        assertEquals("O campo password é obrigatório!", exception.getMessage());
+    }
 }
