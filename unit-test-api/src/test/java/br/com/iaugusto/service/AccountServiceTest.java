a3a20b2c0e45d1f685c443d0dbe9ee3997e32b6b
Igor Augusto <igoraugustobz@gmail.com>
2024-01-20 09:51:07 -0300

    test: deve salvar segunda conta mesmo já existindo outras

--- unit-test-api/src/test/java/br/com/iaugusto/service/AccountServiceTest.java
+++ unit-test-api/src/test/java/br/com/iaugusto/service/AccountServiceTest.java
@@ -1,6 +1,7 @@
 package br.com.iaugusto.service;
 
 import br.com.iaugusto.domain.Account;
+import br.com.iaugusto.domain.exceptions.ValidationException;
 import br.com.iaugusto.service.repositories.AccountRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,8 +10,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static br.com.iaugusto.domain.builders.AccountBuilder.umaAccount;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -22,8 +26,8 @@ class AccountServiceTest {
     private AccountService accountService;
 
     @Test
-    @DisplayName("Deve salvar uma conta com sucesso")
-    void mustSaveAccountWithSuccessfully() {
+    @DisplayName("Deve salvar primeira conta com sucesso")
+    void mustSaveFirstAccountWithSuccessfully() {
         Account accountToSave = umaAccount().comId(null).agora();
 
         when(accountRepository.save(accountToSave)).thenReturn(umaAccount().agora());
@@ -32,6 +36,19 @@ class AccountServiceTest {
 
         assertNotNull(savedAccount.getId());
         verify(accountRepository).save(accountToSave);
+    }
+
+    @Test
+    @DisplayName("Deve salvar segunda conta mesmo já existindo outras")
+    void mustSaveSecondAccountEveIfOthersAlreadyExist() {
+        Account accountToSave = umaAccount().comId(null).agora();
+
+        when(accountRepository.getAccountByUser(accountToSave.getUser().getId()))
+                .thenReturn(Arrays.asList(umaAccount().comName("Outra conta").agora()));
+        when(accountRepository.save(accountToSave)).thenReturn(umaAccount().agora());
+
+        Account savedAccount = accountService.save(accountToSave);
+        assertNotNull(savedAccount.getId());
     }
 }
 
